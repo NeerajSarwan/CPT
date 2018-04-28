6e704bf2dbd2fceefc0d54cbbf42d9b9dd86c961
Neeraj Singh Sarwan <nsingh.sarwan.min13@itbhu.ac.in>
2018-04-29 00:46:44 +0530

    change example according to new structure

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from CPT import *
 model = CPT()
-data,target = model.load_files("./data/train.csv","./data/test.csv")
-model.train(data)
-predictions = model.predict(data,target,5,3)
+train,test = model.load_files("./data/train.csv","./data/test.csv",merge = True)
+model.train(train)
+predictions = model.predict(train,test,5,3)
